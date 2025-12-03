dd328e3ae2c4675ca213c6dc4b22308bcd40d5d8
evgen-kaz <itroute@mail.ru>
2025-12-03 09:55:31 +0800

    fix

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,23 +1,22 @@
 package tests;
 
+import Screen.SearchScreen;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static io.appium.java_client.AppiumBy.accessibilityId;
-import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
-
 public class SearchTests extends TestBase {
+
+    SearchScreen searchScreen = new SearchScreen();
 
     @Test
     void successfulSearchTest() {
-        step("Type search", () -> {
-            $(id("org.wikipedia.alpha:id/bottomOffset")).click();
-            $(id("org.wikipedia.alpha:id/search_container")).sendKeys("Appium"); //имено нужно из java_client.AppiumBy.*;
+        step("Ввод слова 'Тестирование' в поисковую строку", () -> {
+            searchScreen.clickInputSearch();
+            searchScreen.inputTextInSearchInput("Тестирование");
         });
-        step("Verify content found", () ->
-                $$(id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(sizeGreaterThan(0)));
+
+        step("Проверка наличия искомого текста в результатах поиска", () -> {
+            searchScreen.searchInputByText();
+            searchScreen.checkResultSearchText();
+        });
     }
 }
